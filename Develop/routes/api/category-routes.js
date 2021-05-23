0c4bc5c881263fcdea8a7bcb2d0d9df6c44770f8
wael ahmed <w.ahmed020@gmail.com>
2021-05-23 23:09:43 +0100

    router get update for category by id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -8,20 +8,34 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({include: [{model: Product}]});
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  
+try {
+  const categoryData = await Category.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  });
+  if (!categoryData) {
+    res.status(404), json({ message: 'No Category Found!' });
+    return;
+  }
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
