d0b4c571bc2f8f894517a8534b52944143226c8e
wael ahmed <w.ahmed020@gmail.com>
2021-05-23 22:20:54 +0100

    cat route get code updated

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,11 +6,12 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll({
-include: [Product]
-  }).then((categories)=> {
-    res.json(categories)
-  })
+  try {
+    const categoryData = await Category.findAll({include: [{model: Product}]});
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
