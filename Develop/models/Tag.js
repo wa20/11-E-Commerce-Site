48ca819fc1ca15f0e8ffbb6f66affd0aaeb6b4ce
wael ahmed <w.ahmed020@gmail.com>
2021-05-23 13:41:47 +0100

    tag js updated

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -13,11 +13,12 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true
     },
+
     tag_name: {
       type: DataTypes.STRING,
-    
     }
   },
+  
   {
     sequelize,
     timestamps: false,
