d4681bd5a4069b687798b6c3318cd0ff6fe80fe1
wael ahmed <w.ahmed020@gmail.com>
2021-05-23 22:25:15 +0100

    async added to get

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
@@ -17,6 +17,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
