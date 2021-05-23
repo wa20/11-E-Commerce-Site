81e52439f2890a0172dfa383f7921a448744f716
wael ahmed <w.ahmed020@gmail.com>
2021-05-23 23:37:27 +0100

    cat route delete code added

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -56,10 +56,6 @@ router.delete('/:id', (req, res) => {
     const categoryData = await Category.destroy({
       where: {category_id: req.params.id,},
     });
-    if (!categoryData) {
-      res.status(404).json({ message: 'ID not found' });
-      return;
-    }
     res.status(200).json({ message: 'Deleted! Category' });
   } catch (err) {
     res.status(500).json(err);
