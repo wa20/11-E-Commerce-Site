aeee161e83743aea6458996d46c1260f0da46172
wael ahmed <w.ahmed020@gmail.com>
2021-05-24 17:36:39 +0100

    readme updated

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -51,7 +51,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
