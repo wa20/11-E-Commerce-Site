dd97b0fc2ae46e4f577cb2bcd258eb9c2c5ad3c6
wael ahmed <w.ahmed020@gmail.com>
2021-05-25 10:46:03 +0100

    cat route code updates

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -48,7 +48,7 @@ router.put('/:id', async (req, res) => {
     },
     {where: { category_id: req.params.id },
     });
-    res.status(200).json({ message: `Updated Category` });
+    res.status(200).json({categoryData});
   } catch (err) {
     res.status(500).json(err);
   }
@@ -61,7 +61,7 @@ router.delete('/:id', async (req, res) => {
     const categoryData = await Category.destroy({
       where: {category_id: req.params.id,},
     });
-    res.status(200).json({ message: 'Deleted Category' });
+    res.status(200).json({categoryData});
   } catch (err) {
     res.status(500).json(err);
   }
