8d09717d3ca66643734307a04eed1539a50e66ed
wael ahmed <w.ahmed020@gmail.com>
2021-05-25 11:09:25 +0100

    cat route code fixes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -43,10 +43,8 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update({
-      category_name: req.body.category_name
-    },
-    {where: { category_id: req.params.id },
+    const categoryData = await Category.update(req.body,
+    {where: {id: req.params.id },
     });
     res.status(200).json({categoryData});
   } catch (err) {
@@ -59,7 +57,7 @@ router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
-      where: {category_id: req.params.id,},
+      where: {id: req.params.id,},
     });
     res.status(200).json({categoryData});
   } catch (err) {
